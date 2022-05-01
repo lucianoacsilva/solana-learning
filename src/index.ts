19bb356dd5981c6713d3d1b9148fdd582520ae8b
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2022-05-01 12:35:47 -0300

    refactor: Changes function calls in index root file

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,17 @@
-import {  Connection,
-    PublicKey,
-    clusterApiUrl,
-    Keypair,
-    LAMPORTS_PER_SOL
-} from "@solana/web3.js";
+import {  Keypair } from "@solana/web3.js";
+import getWalletBalance from "./utils/getBalance";
+import airDropSol from "./utils/airDrop";
 
 const wallet: Keypair = new Keypair();
 
 const publicKey = wallet.publicKey;
 const secretKey = wallet.secretKey;
 
-const getWalletBalance = async () => {
-    try {
-        const connection: Connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
-        const walletBalance: number = await connection.getBalance(publicKey);
-
-        console.log('walletBalance :>> ', walletBalance);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const airDropSol = async() => {
-    try {
-        const connection: Connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
-        const fromAirDropSignature: string = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL);
-
-        await connection.confirmTransaction(fromAirDropSignature);
-    } catch (error) {
-        console.log(error);
-    }
-}
 const main = async () => {
-    await getWalletBalance();
-    await airDropSol();
+    await getWalletBalance(publicKey);
+    await airDropSol(publicKey);
     
-    await getWalletBalance();
+    await getWalletBalance(publicKey);
 }
 
 main();
