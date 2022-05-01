ee0d99af19a487af81d4fcf64dabb31cd3df6a0a
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2022-05-01 12:19:32 -0300

    feature: Verifies balance

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,20 @@ const wallet: Keypair = new Keypair();
 const publicKey = wallet.publicKey;
 const secretKey = wallet.secretKey;
 
+const getWalletBalance = async () => {
+    try {
+        const connection: Connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+        const walletBalance: number = await connection.getBalance(publicKey);
+
+        console.log('walletBalance :>> ', walletBalance);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const main = async () => {
+    await getWalletBalance();
+
+}
+
+main();
